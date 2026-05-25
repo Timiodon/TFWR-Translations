65b0dfba63a73e42c81bbb29369287fbda93ba79
Zoroark-Zwart <251644139+Zoroark-Zwart@users.noreply.github.com>
2026-05-25 17:59:30 -0400

    Improved import section and explanations

--- builtins.py
+++ builtins.py
@@ -6,13 +6,23 @@
 
 # Expose some useful types to allow for typing without using a typing import.
 # Typing imports would fail to run in-game as they are not ignored.
+# Ignore any type warnings as needed by your environment.
+# Some types are provided as private to the let file have control
+# over the regular defaults and are not commonly useful for typing most code
 
 # Notes on aliases because of TFWR functions:
 # - string -> builtins.str
 # - range_class -> builtins.range
 
-from typing import Any, Self, TypeVar, Literal, Final, overload, Never
-from collections.abc import Callable, Iterable, Iterator, Sequence, Container
+from typing import (
+    Any, Literal, Final,
+    overload, Self, Never
+)
+from collections.abc import (
+    Callable, Iterator, Iterable,
+    Sequence, Container, Collection
+)
+from types import ModuleType
 
 from builtins import (
     bool, int, float, str as string,
@@ -20,10 +30,9 @@ from builtins import (
 
     # If you uncomment the custom classes found below then
     # comment this line to prevent conflicts
-	range as range_class,
+    range as range_class,
     list, set, dict
 )
-from types import ModuleType
 
 # Used for when the builtin type is desirable over a possible
 # redefinition using the same name
